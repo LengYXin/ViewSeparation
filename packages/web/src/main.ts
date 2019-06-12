cee450e5b7252c7c87340b345ae5c619c8e26e5a
LengYXin <lengyingxin8966@gmail.com>
2019-06-12 11:28:41 +0800

    update

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,14 +1,11 @@
 import Vue from 'vue'
-import App from './app/index.vue'
-import router from './router'
-import store from './store'
+import App from '@/app/index.vue'
+import router from './app/router'
 import './registerServiceWorker'
 import './style/core/base.scss'
-
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
