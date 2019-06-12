d87a5f561956fdaf6515b43df8581bc1d5eeecde
LengYXin <lengyingxin8966@gmail.com>
2019-06-12 12:37:38 +0800

    test

--- packages/www/src/pages/test/index.test.tsx
+++ packages/www/src/pages/test/index.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './index.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import App from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BrowserRouter> <App /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
